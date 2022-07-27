d67c5a674db9c9b7471c16883442e4a5bbb631e5
DESKTOP-AQP88U3\paulo <pauloandre232@gmail.com>
2022-07-26 23:00:22 -0300

    avatar service impl

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,10 +1,10 @@
 import AppError from '@shared/errors/AppError';
 import { IUserRepository } from '../infra/repositories/IUserRepository';
-import User from '../infra/typeorm/entities/User';
 import { inject, injectable } from 'tsyringe';
 import path from 'path';
 import uploadConfig from '@config/upload';
 import fs from 'fs';
+import User from '../infra/typeorm/entities/User';
 
 interface IRequest {
   user_id: string;
@@ -18,7 +18,7 @@ class UpdateUserAvatarService {
     private repository: IUserRepository,
   ) {}
 
-  public async execute({ user_id, avatarFilename }: IRequest): Promise<void> {
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
     const user = await this.repository.findById(user_id);
 
     if (!user) {
@@ -28,10 +28,17 @@ class UpdateUserAvatarService {
     if (user.avatar) {
       const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
       const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+      if(userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+    user.avatar = avatarFilename;
 
+    await this.repository.save(user);
 
+    return user;
   }
-}
+
 }
 
 export default UpdateUserAvatarService;
